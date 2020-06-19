0cb63a71b23136e7896d7fcdb0a5ea27b383f7a3
Jean Pierre <ingramosjean@gmail.com>
2020-06-19 12:26:33 -0500

    SETUP DAY 5 SIGNUP

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import './App.css';
 import Homepage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
-import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import {auth} from './firebase/firebase.utils';
+import {auth, createUserProfileDocument} from './firebase/firebase.utils';
+import SigninAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 class App extends React.Component{
   constructor(){
@@ -21,10 +21,20 @@ class App extends React.Component{
   unsubscribeFromAuth = null
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user =>{
-      this.setState({currentUser: user});
-
-      console.log(user);
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth =>{
+      if(userAuth){
+        const userRef = await createUserProfileDocument(userAuth);
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser:{
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          });
+        });
+      }else{
+      this.setState({currentUser: userAuth});
+      }
     });
   }
 
@@ -39,7 +49,7 @@ class App extends React.Component{
         <Switch>
           <Route exact path='/' component = {Homepage}/>
           <Route path='/shop' component = {ShopPage}/>
-          <Route path='/signin' component = {SignInAndSignUp}/>
+          <Route path='/signin' component = {SigninAndSignUp}/>
         </Switch>
       </div>
     );
