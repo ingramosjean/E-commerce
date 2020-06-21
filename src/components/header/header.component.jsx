36689d393907a99e95aff9cb03008cdcf28709da
Jean Pierre <ingramosjean@gmail.com>
2020-06-21 12:53:28 -0500

    DAY 6 CART 1

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,7 +3,8 @@ import {Link} from 'react-router-dom';
 import {connect} from 'react-redux';
 import {auth} from '../../firebase/firebase.utils';
 import {ReactComponent as Logo} from '../../assets/crown.svg';
-
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 import './header.styles.scss';
 
 const Header = ({currentUser}) => (
@@ -26,7 +27,9 @@ const Header = ({currentUser}) => (
                     SIGN IN
                 </Link>
             }
+            <CartIcon/>
         </div>
+        <CartDropdown/>
     </div>
 );
 
