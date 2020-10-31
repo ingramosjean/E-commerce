6aea31e73e444966fbd6c8f208ce032f19c819d0
Jean Pierre <ingramosjean@gmail.com>
2020-10-31 09:46:21 -0500

    SignIn update Sagas

--- src/App.js
+++ src/App.js
@@ -7,16 +7,13 @@ import {createStructuredSelector} from 'reselect';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
-import {auth, createUserProfileDocument} from './firebase/firebase.utils';
 import SigninAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import {setCurrentUser} from './redux/user/user.actions';
 import CheckoutPage from './pages/checkout/checkout.component';
 
 class App extends React.Component{
-  unsubscribeFromAuth = null
+  unsubscribeFromAuth = null;
 
   componentDidMount() {
-    const{setCurrentUser} = this.props;
 
     //this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth =>{
     //  if(userAuth){
@@ -59,11 +56,6 @@ const mapStateToProps = createStructuredSelector ({
   currentUser: selectCurrentUser
 });
 
-const mapDispatchToProps = dispatch => ({
-  setCurrentUser: user =>dispatch(setCurrentUser(user))
-});
-
 export default connect(
-  mapStateToProps,
-  mapDispatchToProps)
+  mapStateToProps)
   (App);
